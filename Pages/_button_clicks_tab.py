5bc951bc70bb1c1f68cb666d6ba3da6f69c38a5f
Neil <bagabagon111000@gmail.com>
2026-06-18 15:28:28 +0800

    Added different Button Clicks (WebElement | JavaScript | Action Move & Click)

---
+++ Pages/_button_clicks_tab.py
@@ -0,0 +1,112 @@
+import time
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from window_utils import WindowUtils
+
+class ButtonClickTab:
+    def __init__(self, driver):
+        self.driver = driver
+        self.url = "https://webdriveruniversity.com/"
+
+    def open(self):
+        self.driver.get("https://webdriveruniversity.com/")
+
+    # Using WebElement.click()
+    def button_dot_clicks(self):
+        button = self.driver.find_element(By.XPATH, "//a[@id='button-clicks']")
+        main_window = self.driver.current_window_handle
+
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", button)
+        time.sleep(1)
+        self.driver.execute_script("arguments[0].click();", button)
+        WindowUtils.driver_switch_wait(self.driver)
+
+
+        self.driver.find_element(By.XPATH, "//span[@id='button1']").click()
+        time.sleep(1)
+        button_close = self.driver.find_element(By.XPATH, "//div[@id='myModalClick']//button[@type='button'][normalize-space()='Close']")
+        button_close.click()
+        print("\nButton Clicked (WebEl): ", button_close.text)
+        time.sleep(1)
+        self.driver.find_element(By.XPATH, "//span[@id='button1']").click()
+        time.sleep(1)
+        button_x = self.driver.find_element(By.XPATH, "//div[@id='myModalClick']//button[@type='button'][normalize-space()='×']")
+        button_x.click()
+        print("Button Clicked (WebEl): ", button_x.text)
+        time.sleep(1)
+        self.driver.close()
+        time.sleep(1)
+        self.driver.switch_to.window(main_window)
+        time.sleep(1)
+        self.driver.refresh()
+        self.driver.execute_script("window.scrollTo(0, 0);")
+        time.sleep(3)
+
+    # Using JavaScript click
+    def button_js_clicks(self):
+        button = self.driver.find_element(By.XPATH, "//a[@id='button-clicks']")
+        main_window = self.driver.current_window_handle
+
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", button)
+        time.sleep(1)
+        self.driver.execute_script("arguments[0].click();", button)
+        WindowUtils.driver_switch_wait(self.driver)
+
+        button2 = self.driver.find_element(By.CSS_SELECTOR, "#button2")
+
+        time.sleep(1)
+        self.driver.execute_script("arguments[0].click();", button2)
+        time.sleep(1)
+
+        button_close = self.driver.find_element(By.CSS_SELECTOR, "div[class='modal-dialog modal-md'] div[class='modal-footer'] button[type='button']")
+        self.driver.execute_script("arguments[0].click();", button_close)
+        print("\nButton Clicked (JS): ", button_close.text)
+        time.sleep(1)
+
+        self.driver.execute_script("arguments[0].click();", button2)
+        time.sleep(1)
+
+        button_x = self.driver.find_element(By.CSS_SELECTOR, "div[class='modal-dialog modal-md'] div[class='modal-header'] button[type='button']")
+        self.driver.execute_script("arguments[0].click();", button_x)
+        print("Button Clicked (JS): ", button_x.text)
+        time.sleep(1)
+
+        self.driver.close()
+        time.sleep(1)
+        self.driver.switch_to.window(main_window)
+        time.sleep(1)
+
+    # Using Action Move & Click
+    def button_action_move_click(self):
+        button = self.driver.find_element(By.XPATH, "//a[@id='button-clicks']")
+        main_window = self.driver.current_window_handle
+
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", button)
+        time.sleep(1)
+        self.driver.execute_script("arguments[0].click();", button)
+        WindowUtils.driver_switch_wait(self.driver)
+
+        move_action = ActionChains(self.driver)
+
+        def open_click_me():
+            time.sleep(1)
+            button_click_me = self.driver.find_element(By.XPATH, "//span[@id='button3']")
+            move_action.move_to_element(button_click_me).click().perform()
+            time.sleep(1)
+
+        open_click_me()
+        button_close = self.driver.find_element(By.XPATH, "//div[@id='myModalMoveClick']//button[@type='button'][normalize-space()='Close']")
+        move_action.move_to_element(button_close).click().perform()
+        print("\nButton Clicked (ActMove): ", button_close.text)
+        time.sleep(1)
+
+        open_click_me()
+        button_x = self.driver.find_element(By.XPATH, "//div[@id='myModalMoveClick']//button[@type='button'][normalize-space()='×']")
+        move_action.move_to_element(button_x).click().perform()
+        print("Button Clicked (ActMove): ", button_x.text)
+        time.sleep(1)
+
+        self.driver.close()
+        time.sleep(1)
+        self.driver.switch_to.window(main_window)
+        time.sleep(1)
---
+++ Pages/_login_tab.py
@@ -0,0 +1,50 @@
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
+from window_utils import WindowUtils
+from selenium.webdriver.support import expected_conditions as EC
+
+class LoginTab:
+    def __init__(self, driver):
+        self.driver = driver
+        self.url = "https://webdriveruniversity.com/"
+
+    def open(self):
+        self.driver.get("https://webdriveruniversity.com/")
+
+    def login_portal(self):
+        def input_succeed():
+            self.driver.find_element(By.XPATH, "//input[@id='text']").send_keys("webdriver")
+            self.driver.find_element(By.XPATH, "//input[@id='password']").send_keys("webdriver123")
+
+        def input_failed():
+            self.driver.find_element(By.XPATH, "//input[@id='text']").send_keys("izkitzo")
+            self.driver.find_element(By.XPATH, "//input[@id='password']").send_keys("izkitzo123")
+
+        button = self.driver.find_element(By.XPATH, "//a[@id='login-portal']")
+
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", button)
+        self.driver.execute_script("arguments[0].click();", button)
+        WindowUtils.driver_switch_wait(self.driver)
+
+        input_failed()
+        self.driver.find_element(By.XPATH, "//button[@id='login-button']").click()
+        alert = WebDriverWait(self.driver, 5).until(EC.alert_is_present())
+        assert alert.text == "validation failed"
+        print("\n", alert.text)
+        time.sleep(1)
+        alert.accept()
+        time.sleep(1)
+
+        input_succeed()
+        self.driver.find_element(By.XPATH, "//button[@id='login-button']").click()
+        alert = WebDriverWait(self.driver, 5).until(EC.alert_is_present())
+        assert alert.text == "validation succeeded"
+        print("\n", alert.text)
+        time.sleep(1)
+        alert.accept()
+        time.sleep(1)
+
+        self.driver.find_element(By.XPATH, "//a[@id='home-link']").click()
+        time.sleep(1)
